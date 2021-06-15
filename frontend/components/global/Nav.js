f166d3698f33a4893ee44e94d89d01d6beaad4c2
MBJean <michaelbrianjean@gmail.com>
2021-06-15 16:43:35 -0400

    Updated webpack config to introduce file-loader and updated docstring/naming conventions throughout

--- frontend/components/global/Nav.js
+++ frontend/components/global/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import STYLES from "./Nav.module.scss";
+import DH_LOGO from "../../images/dh_logo.svg";
 
 const Nav = () => {
 
@@ -9,7 +10,7 @@ const Nav = () => {
                 App Title
             </a>
             <a className={STYLES.linkLab} href="https://digitalhumanities.mit.edu/" target="_blank" rel="noreferrer">
-                <img className={STYLES.imageLab} src="/static/img/dh_logo.svg" />
+                <img className={STYLES.imageLab} src={DH_LOGO} />
             </a>
         </nav>
     );
