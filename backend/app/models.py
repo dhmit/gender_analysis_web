f6434188d9e57f6b6622a99fbc742e0d2c01e043
Peihua Huang <nemesis2126@gmail.com>
2021-06-17 16:37:59 -0400

    moved functions into document class

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,6 +2,9 @@
 Models for the ***** app.
 """
 from django.db import models
+import nltk
+import string
+from collections import Counter
 
 # no models yet -- write me!
 
@@ -20,3 +23,75 @@ class Document(models.Model):
     _word_counts_counter = models.JSONField(null=True, blank=True, default=dict)
     _part_of_speech_tags = models.JSONField(null=True, blank=True, default=list)
 
+    def _clean_quotes(self):
+        """
+        Scans through the text and replaces all of the smart quotes and apostrophes with their
+        "normal" ASCII variants
+
+        :param text: The string to reformat
+        :return: A string that is idential to `text`, except with its smart quotes exchanged
+        """
+
+        # Define the quotes that will be swapped out
+        smart_quotes = {
+            '“': '"',
+            '”': '"',
+            "‘": "'",
+            "’": "'",
+        }
+
+        # Replace all entries one by one
+        output_text = self.text
+        for quote in smart_quotes:
+            output_text = output_text.replace(quote, smart_quotes[quote])
+        self.text = output_text
+        self.save()
+        return self.text
+
+
+    def get_tokenized_text(self):
+        """
+        Tokenizes the text and returns it as a list of tokens, while removing all punctuation.
+
+        Note: This does not currently properly handle dashes or contractions.
+
+        :return: List of each word in the Document
+        """
+
+        # Excluded characters: !"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+        if self.tokenized_text is None:
+            tokens = nltk.word_tokenize(self.text)
+            excluded_characters = set(string.punctuation)
+            tokenized_text = [word.lower() for word in tokens if word not in excluded_characters]
+            self.tokenized_text = tokenized_text
+            self.save()
+        return self.tokenized_text
+
+    def get_word_count(self):
+        """
+        Lazy-loading for **Document.word_count** attribute.
+        Returns the number of words in the document.
+        The word_count attribute is useful for the get_word_freq function.
+        However, it is performance-wise costly, so it's only loaded when it's actually required.
+
+        :return: Number of words in the document's text as an int
+
+        """
+        if self.word_count is None:
+            self.word_count = len(self.get_tokenized_text())
+            self.save()
+        return self.word_count
+
+    def get_wordcount_counter(self):
+        """
+        Returns a counter object of all of the words in the text.
+
+        If this is your first time running this method, this can be slow.
+
+        :return: Python Counter object
+        """
+
+        # If word_counts were not previously initialized, do it now and store it for the future.
+        if not self._word_counts_counter:
+            self._word_counts_counter = Counter(self.get_tokenized_text())
+        return self._word_counts_counter
--- backend/app/tests.py
+++ backend/app/tests.py
@@ -5,7 +5,6 @@ Tests for the main app.
 from django.test import TestCase
 from collections import Counter
 from .models import Document
-from .analysis import document
 
 
 class MainTests(TestCase):
@@ -23,25 +22,31 @@ class MainTests(TestCase):
         doc_2 = Document(text='She really likes to eat chocolate!')
         doc_2.save()
         tokenized_text_2 = ['she', 'really', 'likes', 'to', 'eat', 'chocolate']
-        self.assertEqual(document.get_tokenized_text(doc_1), tokenized_text_1)
-        self.assertEqual(document.get_tokenized_text(doc_2), tokenized_text_2)
+        self.assertEqual(doc_1.get_tokenized_text(), tokenized_text_1)
+        self.assertEqual(doc_2.get_tokenized_text(), tokenized_text_2)
 
-    def test_word_count(self):
+    def test_get_word_count(self):
         doc_1 = Document(text='The quick brown fox jumped over the lazy dog.')
         doc_1.save()
         word_count_1 = 9
         doc_2 = Document(text='She really likes to eat chocolate!')
         doc_2.save()
         word_count_2 = 6
-        self.assertEqual(document.word_count(doc_1), word_count_1)
-        self.assertEqual(document.word_count(doc_2), word_count_2)
+        self.assertEqual(doc_1.get_word_count(), word_count_1)
+        self.assertEqual(doc_2.get_word_count(), word_count_2)
 
-    def test_get_word_counts_counter(self):
+    def test_get_wordcount_counter(self):
         doc_1 = Document(text='Do you like ice cream as much as I do?')
         doc_1.save()
         counter_1 = Counter({'do': 2, 'you': 1, 'like': 1, 'ice': 1, 'cream': 1, 'as': 2, 'much': 1, 'i': 1})
         doc_2 = Document(text='She really likes to eat chocolate!')
         doc_2.save()
         counter_2 = Counter({'she': 1, 'really': 1, 'likes': 1, 'to': 1, 'eat': 1, 'chocolate': 1})
-        self.assertEqual(document.get_wordcount_counter(doc_1), counter_1)
-        self.assertEqual(document.get_wordcount_counter(doc_2), counter_2)
+        self.assertEqual(doc_1.get_wordcount_counter(), counter_1)
+        self.assertEqual(doc_2.get_wordcount_counter(), counter_2)
+
+    def test_clean_quotes(self):
+        doc_1 = Document(text='This is a “smart” phrase')
+        doc_1.save()
+        cleaned_1 = 'This is a "smart" phrase'
+        self.assertEqual(doc_1._clean_quotes(), cleaned_1)
