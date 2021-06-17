da5a3ea5e2d70fe8aeefd65aba8996a8c2baba45
Peihua Huang <nemesis2126@gmail.com>
2021-06-17 14:53:30 -0400

    update indentation and moved get_tokenized_text to backend

--- backend/app/analysis/document.py
+++ backend/app/analysis/document.py
@@ -9,28 +9,28 @@ import nltk
 
 def word_count(document_obj):
     """
-        Lazy-loading for **Document.word_count** attribute.
-        Returns the number of words in the document.
-        The word_count attribute is useful for the get_word_freq function.
-        However, it is performance-wise costly, so it's only loaded when it's actually required.
+    Lazy-loading for **Document.word_count** attribute.
+    Returns the number of words in the document.
+    The word_count attribute is useful for the get_word_freq function.
+    However, it is performance-wise costly, so it's only loaded when it's actually required.
 
-        :return: Number of words in the document's text as an int
+    :return: Number of words in the document's text as an int
 
-        >>> from gender_analysis import Document
-        >>> from pathlib import Path
-        >>> from gender_analysis.testing.common import TEST_DATA_DIR
-        >>> document_metadata = {'author': 'Austen, Jane', 'title': 'Persuasion', 'date': '1818',
-        ...                      'filename': 'austen_persuasion.txt',
-        ...                      'filepath': Path(TEST_DATA_DIR, 'sample_novels',
-        ...                                       'texts', 'austen_persuasion.txt')}
-        >>> austen = Document(document_metadata)
-        >>> austen.word_count
-        83285
+    >>> from gender_analysis import Document
+    >>> from pathlib import Path
+    >>> from gender_analysis.testing.common import TEST_DATA_DIR
+    >>> document_metadata = {'author': 'Austen, Jane', 'title': 'Persuasion', 'date': '1818',
+    ...                      'filename': 'austen_persuasion.txt',
+    ...                      'filepath': Path(TEST_DATA_DIR, 'sample_novels',
+    ...                                       'texts', 'austen_persuasion.txt')}
+    >>> austen = Document(document_metadata)
+    >>> austen.word_count
+    83285
 
-        """
-        if document_obj._word_count is None:
-            document_obj._word_count = len(document_obj.get_tokenized_text())
-        return document_obj._word_count
+    """
+    if document_obj._word_count is None:
+        document_obj._word_count = len(document_obj.get_tokenized_text())
+    return document_obj._word_count
 
 
 def _clean_quotes(text):
@@ -61,3 +61,41 @@ def _clean_quotes(text):
         output_text = output_text.replace(quote, smart_quotes[quote])
 
     return output_text
+
+def get_tokenized_text(document_obj):
+    """
+    Tokenizes the text and returns it as a list of tokens, while removing all punctuation.
+
+    Note: This does not currently properly handle dashes or contractions.
+
+    :return: List of each word in the Document
+
+    >>> from gender_analysis import Document
+    >>> from pathlib import Path
+    >>> from gender_analysis.testing.common import TEST_DATA_DIR
+    >>> document_metadata = {'author': 'Austen, Jane', 'title': 'Persuasion', 'date': '1818',
+    ...                      'filename': 'test_text_1.txt',
+    ...                      'filepath': Path(TEST_DATA_DIR,
+    ...                                       'document_test_files', 'test_text_1.txt')}
+    >>> austin = Document(document_metadata)
+    >>> tokenized_text = austin.get_tokenized_text()
+    >>> tokenized_text
+    ['allkinds', 'of', 'punctuation', 'and', 'special', 'chars']
+
+    """
+
+    # Excluded characters: !"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+    if document_obj._tokenized_text is None:
+
+        excluded_characters = set(string.punctuation)
+        cleaned_text = ''
+        for character in document_obj.text:
+            if character not in excluded_characters:
+                cleaned_text += character
+
+        tokenized_text = cleaned_text.lower().split()
+        document_obj._tokenized_text = tokenized_text
+        return tokenized_text
+
+    else:
+        return document_obj._tokenized_text
