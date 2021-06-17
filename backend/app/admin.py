b0ff90b2109be0fa7167b66aa90fd7c0aed3ee88
Joshua Feliciano <joshfeli613@gmail.com>
2021-06-17 13:22:47 -0400

    Created custom LowercaseCharField field
    Created new field to preserve the case-insensitive properties of the subj and obj attributes of the PronounSeries class (model).  Description for LowercaseCharField is a work in progress (how do we access the max_length?) Also slightly reformatted models_to_register list in admin.py.

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -5,7 +5,10 @@ This file controls the administrative interface for gender analysis web app
 from django.contrib import admin
 from . import models
 
-models_to_register = [models.PronounSeries, models.Gender]
+models_to_register = [
+    models.PronounSeries,
+    models.Gender,
+]
 
 for model in models_to_register:
     admin.site.register(model)
--- backend/app/models.py
+++ backend/app/models.py
@@ -2,6 +2,30 @@
 Models for the gender analysis web app.
 """
 from django.db import models
+# from django.utils.translation import gettext_lazy
+
+
+class LowercaseCharField(models.CharField):
+    """
+    A subclass of models.CharField that converts the string to lowercase
+    when saving the model instance to the database.
+    """
+
+    # WORK IN PROGRESS!
+    # @property
+    # def description(self):
+    #     return gettext_lazy(f"String (up to {self.max_length}) \
+    #         converted to lowercase upon saving the model instance to the database.")
+
+    def pre_save(self, model_instance, add):
+        """
+        Preprocesses field data before saving a model instance to the database.
+        In this case, converts the data described by the CharField to lowercase.
+        """
+        chars = getattr(model_instance, self.attname)
+        setattr(model_instance, self.attname, chars.lower())
+
+        return super().pre_save(model_instance, add)
 
 
 class PronounSeries(models.Model):
@@ -10,9 +34,9 @@ class PronounSeries(models.Model):
     `gender_analysis` functions
     """
 
-    identifier = models.CharField(max_length=40)
-    subj = models.CharField(max_length=100)
-    obj = models.CharField(max_length=100)
+    identifier = models.CharField(max_length=60)
+    subj = models.LowercaseCharField(max_length=40)
+    obj = models.LowercaseCharField(max_length=40)
 
     def __init__(self, identifier, pronouns, subj, obj):
         """
