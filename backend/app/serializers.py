db7c0c1cca01a4984bbe3fb7ed4ddaf58e202abb
Joshua Feliciano <joshfeli613@gmail.com>
2021-06-21 20:06:21 -0400

    Added a Pronoun serializer in serializers.py

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -4,8 +4,13 @@ in ways that can be transported across the backend/frontend divide, or
 allow the frontend to suggest changes to the backend/database.
 """
 # import json
-# from rest_framework import serializers
-# from .models import ()
+from rest_framework import serializers
+from .models import (
+    Pronoun,
+)
 
 
-# no serializers yet -- write me!
+class PronounSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pronoun
+        fields = ['id', 'identifier', 'type']
