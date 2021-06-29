264783ddf074f488510cf7470fb91337c762ab8e
Joshua Feliciano <joshfeli613@gmail.com>
2021-06-29 15:00:40 -0400

    Added a PronounSeries serializer

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -6,14 +6,26 @@ allow the frontend to suggest changes to the backend/database.
 # import json
 from rest_framework import serializers
 from .models import (
+    PronounSeries,
     Document,
 )
+
+
+class PronounSeriesSerializer(serializers.ModelSerializer):
+    """
+    Serializes a PronounSeries object
+    """
+
+    class Meta:
+        model = PronounSeries
+        fields = ['identifier', 'subj', 'obj', 'pos_det', 'pos_pro', 'reflex']
 
 
 class DocumentSerializer(serializers.ModelSerializer):
     """
     Serializes a Document object
     """
+
     class Meta:
         model = Document
         fields = ['id', 'author', 'title', 'year', 'text', 'word_count']
