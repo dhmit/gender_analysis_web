f1a31d59a9e382681b7d5fb0897bdfbb595ebfe3
irinazoccolini <irinaz@mit.edu>
2021-06-30 16:27:13 -0400

    Added initial common variables
    Co-Authored-By: Ashar Farooq <ashar744@gmail.com>

--- backend/app/common.py
+++ backend/app/common.py
@@ -1,8 +1,55 @@
 """
-Miscellaneous utility functions useful throughout the system
+Miscellaneous utility functions and variables useful throughout the system
 """
 from textwrap import dedent
+from app.models import Gender
+from nltk.corpus import stopwords
 
+FEMALE = Gender.objects.all().filter(label="Female")
+MALE = Gender.objects.all().filter(label="Male")
+NONBINARY = Gender.objects.all().filter(label="Neo")
+
+BINARY_GROUP = [FEMALE, MALE]
+TRINARY_GROUP = [FEMALE, MALE, NONBINARY]
+
+SWORDS_ENG = stopwords.words('english')
+
+NLTK_TAGS = {
+    'CC': 'conjunction, coordinating',
+    'CD': 'numeral, cardinal',
+    'DT': 'determiner',
+    'EX': 'existential there',
+    'IN': 'preposition or conjunction, subordinating',
+    'JJ': 'adjective or numeral, ordinal',
+    'JJR': 'adjective, comparative',
+    'JJS': 'adjective, superlative',
+    'LS': 'list item marker',
+    'MD': 'modal auxiliary',
+    'NN': 'noun, common, singular or mass',
+    'NNP': 'noun, proper, singular',
+    'NNS': 'noun, common, plural',
+    'PDT': 'pre-determiner',
+    'POS': 'genitive marker',
+    'PRP': 'pronoun, personal',
+    'PRP$': 'pronoun, possessive',
+    'RB': 'adverb',
+    'RBR': 'adverb, comparative',
+    'RBS': 'adverb, superlative',
+    'RP': 'particle',
+    'TO': '"to" as preposition or infinitive marker',
+    'UH': 'interjection',
+    'VB': 'verb, base form',
+    'VBD': 'verb, past tense',
+    'VBG': 'verb, present participle or gerund',
+    'VBN': 'verb, past participle',
+    'VBP': 'verb, present tense, not 3rd person singular',
+    'VBZ': 'verb, present tense, 3rd person singular',
+    'WDT': 'WH-determiner',
+    'WP': 'WH-pronoun',
+    'WRB': 'Wh-adverb',
+}
+
+NLTK_TAGS_ADJECTIVES = ["JJ", "JJR", "JJS"]
 
 def print_header(header_str):
     """
