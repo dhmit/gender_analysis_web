9956bb6ca167b59b56d480f6ebb73f5a5b986876
Joshua Feliciano <joshfeli613@gmail.com>
2021-07-05 17:08:06 -0400

    Continuing to experiment with memory profiling
    (WORK IN PROGRESS!)
    I've added the `eliot_middlemarch.txt` file and have used it instead of `aarund_longfrock.txt` since the former is 1.7MB while the latter is only a couple of KB. I'm using the `@profile` decorator alongside printing the length of the `connection.queries` list after each line to check where/when I'm hitting the DB as well  as how much memory each hit costs.

--- backend/app/analysis/__init__.py
+++ backend/app/analysis/__init__.py
@@ -7,24 +7,28 @@ from app.models import Document
 def longfrock():
     return open('app/analysis/aanrud_longfrock.txt').read()
 
+def middlemarch():
+    return open('app/analysis/eliot_middlemarch.txt').read()
 
+
+@profile
 def make_documents(num):
     """
     Mechanism (mainly for debugging) for creating test `Document` objects.
     Each `Document` here holds about 2 KB of text.
 
-    :param: num: A nonnegative `int` representing the number of `Document`s to be created and saved to the database.
+    :param: num: A non-negative `int` representing the number of `Document`s to be created and saved to the database.
     :return: `None`
     """
-    assert num >= 0, 'Please input a nonnegative integer.'
+    assert num >= 0, 'Please input a non-negative integer.'
 
     for _ in range(num):
-        Document.objects.create(author='Hans Aanrud', text=longfrock())
+        Document.objects.create(author='George Eliot', title='Middlemarch', text=middlemarch())
 
     print(f'Done! {num} Document objects created.\nTotal number of Document objects: {Document.objects.count()}')
 
 
-@profile
+# @profile
 def run_analysis(doc_set):
     """
     A test version of the `run_analysis` method found in the `GenderProximityAnalyzer` class in `proximity.py`.
@@ -39,26 +43,28 @@ def run_analysis(doc_set):
     results = {}
     # ------- SQLite3 doesn't support the 'DECLARE' or 'FETCH' keywords! (https://www.sqlite.org/lang_keywords.html)
     # ------- It also doesn't support cursors -- we need another solution.
-    cursor = connection.cursor()
-    input_query = str(doc_set.only('tokenized_text').query).replace('\"', '')
-    query = f"DECLARE doc_cursor BINARY CURSOR FOR {input_query}"
+    # cursor = connection.cursor()
+    # input_query = str(doc_set.only('tokenized_text').query).replace('\"', '')
+    # query = f"DECLARE doc_cursor BINARY CURSOR FOR {input_query}"
+    #
+    # breakpoint()  # Works fine up to here
+    #
+    # cursor.execute(query)  # Here's the error
+    # cursor.execute('OPEN doc_cursor')
+    #
+    # while True:
+    #     doc_subset = cursor.execute('FETCH 10 FROM doc_cursor')
+    #     for _ in range(10):
+    #         doc = doc_subset.fetchone()
+    #         breakpoint()
+    print(len(connection.queries))
+    query = doc_set.only('tokenized_text').iterator()
+    print('After iterator declaration:', len(connection.queries))
 
-    breakpoint()  # Works fine up to here
+    for doc in query:
+        print('\nBefore adding to dict:', len(connection.queries))
+        results[doc.pk] = doc.tokenized_text
+        print('After adding to dict:', len(connection.queries))
 
-    cursor.execute(query)  # Here's the error
-    cursor.execute('OPEN doc_cursor')
-
-    while True:
-        doc_subset = cursor.execute('FETCH 10 FROM doc_cursor')
-        for _ in range(10):
-            doc = doc_subset.fetchone()
-            breakpoint()
-
-    # query = doc_set.only('tokenized_text').iterator()
-
-    # for doc in query:
-    #     results['pk'] = 'tokenized_text'
-    #     del results['pk']
-
-    print('END run_analysis')
-    return results
+    print('\nEND run_analysis')
+    return {}
