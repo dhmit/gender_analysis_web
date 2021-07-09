43b892cb99412388d171a3bcf057bc625419da2e
Joshua Feliciano <joshfeli613@gmail.com>
2021-07-09 17:15:22 -0400

    Removed all references to `tags` parameter
    Instead of filtering the results by tag, we run the analysis using all `nltk` tags available and plan to let the user do the filtering on the frontend
    
    Co-Authored-By: Watermelanie <46464201+Watermelanie@users.noreply.github.com>
    Co-Authored-By: irinazoccolini <71565864+irinazoccolini@users.noreply.github.com>
    Co-Authored-By: Ashar Farooq <ashar744@gmail.com>

--- backend/app/analysis/proximity.py
+++ backend/app/analysis/proximity.py
@@ -8,14 +8,13 @@ from ..models import (
 )
 
 
-def run_analysis(doc_ids, gender_ids, tags, word_window):
+def run_analysis(doc_ids, gender_ids, word_window):
     """
     Generates a dictionary of dictionaries for each `Document` object. Each dictionary maps a `Gender` to a word count
     of words within a specified window of that `Gender`'s pronouns.
 
     :param doc_ids: A list of ints representing `Document` ids
     :param gender_ids: A list of ints representing `Gender` ids
-    :param tags: A Python dictionary mapping parts of speech tags to their definitions
     :param word_window: An integer describing the number of words to look at of each side of a gendered word
 
     :return: A dict mapping `Document` ids to a dict mapping strings (`Gender` labels) to a `Counter` instance.
@@ -26,21 +25,19 @@ def run_analysis(doc_ids, gender_ids, tags, word_window):
         results[key] = generate_gender_token_counters(
             Document.objects.values_list('tokenized_text', flat=True).filter(pk=key),
             gender_ids,
-            tags,
             word_window
         )
 
     return results
 
 
-def generate_gender_token_counters(text_query, gender_ids, tags, word_window):
+def generate_gender_token_counters(text_query, gender_ids, word_window):
     """
     Generates a dictionary mapping `Gender`s to a word count of words within a specified window of the `Gender`'s
     pronouns.
 
     :param text_query: An unevaluated, length-1 `QuerySet` that returns a list of strings when evaluated
     :param gender_ids: A list of ints representing `Gender` ids
-    :param tags: A Python dictionary mapping parts of speech tags to their definitions
     :param word_window: An integer describing the number of words to look at of each side of a gendered word
 
     :return: A dict mapping strings (`Gender` labels) to a `Counter` instance.
@@ -50,20 +47,19 @@ def generate_gender_token_counters(text_query, gender_ids, tags, word_window):
     results = {}
 
     for gender_id in gender_ids:
-        doc_result = generate_token_counter(text_query, gender_id, tags, word_window)
+        doc_result = generate_token_counter(text_query, gender_id, word_window)
         results.update({Gender.objects.values_list('label', flat=True).get(pk=gender_id): doc_result})
 
     return results
 
 
-def generate_token_counter(text_query, gender_id, tags, word_window):
+def generate_token_counter(text_query, gender_id, word_window):
     # pylint: disable=too-many-locals
     """
     Generates a 'Counter' instance mapping words to their frequency within a text.
 
     :param text_query: An unevaluated, length-1 `QuerySet` that returns a list of strings when evaluated
     :param gender_id: An int representing a the id of some `Gender` object in the database
-    :param tags: a Python dictionary mapping parts of speech tags to their definitions
     :param word_window: an integer describing the number of words to look at of each side of a gendered word
 
     :return: A 'Counter' instance showcasing the numbered occurrences of words around a gendered pronoun
@@ -83,8 +79,7 @@ def generate_token_counter(text_query, gender_id, tags, word_window):
 
             tagged_tokens = nltk.pos_tag(words)
             for tag_index, _ in enumerate(tagged_tokens):
-                if tagged_tokens[tag_index][1] in tags:
-                    word = words[tag_index]
-                    output[word] += 1
+                word = words[tag_index]
+                output[word] += 1
 
     return output
