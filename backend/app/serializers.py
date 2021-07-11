af662f6b2987108b807020f8a3c218da521e3c8d
Ken Alba <kenalba@bu.edu>
2021-07-11 02:23:47 -0400

    Preliminary frontend stabs
    Just wanted to see if I could figure it out, so I tried to render character lists as a part of the single_document page. It worked! It's definitely Done Wrong, though, we almost certainly want Character react components (?) or something.

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -9,6 +9,8 @@ from .models import (
     PronounSeries,
     Gender,
     Document,
+    Character,
+    Alias,
 )
 
 
@@ -33,20 +35,43 @@ class GenderSerializer(serializers.ModelSerializer):
         fields = ['label', 'pronoun_series', 'pronouns', 'subj', 'obj']
 
 
+class AliasSerializer(serializers.ModelSerializer):
+    """
+    Serializes an Alias object.
+    """
+
+    class Meta:
+        model = Alias
+        fields = ['name', 'count', 'sanitized_pronoun_rates']
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Character object.
+    """
+    aliases = AliasSerializer(many=True, read_only = True)
+
+    class Meta:
+        model = Character
+        fields = ['common_name', 'count', 'full_name', 'gender', 'aliases']
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     """
     Serializes a Document object
     """
+    characters = CharacterSerializer(many=True, read_only=True)
 
     class Meta:
         model = Document
-        fields = ['id', 'author', 'title', 'year', 'text', 'word_count']
+        fields = ['id', 'author', 'title', 'year', 'text', 'word_count', 'characters']
 
 
 class SimpleDocumentSerializer(serializers.ModelSerializer):
     """
     Serializes a Document object (does not include the text itself)
     """
+
     class Meta:
         model = Document
         fields = ['id', 'author', 'title', 'year', 'word_count']
--- frontend/components/SingleDocument.js
+++ frontend/components/SingleDocument.js
@@ -7,6 +7,24 @@ const SingleDocument = ({id}) => {
     const [docData, setDocData] = useState({});
     const [loading, setLoading] = useState(true);
     const [showText, setShowText] = useState(false);
+    const [getCharacters, setGetCharacters] = useState(false);
+
+    function renderGender(gender) {
+        return gender.map((one_gender)=><span key={one_gender[0]}><b>{one_gender[0]}: {one_gender[1]}%   </b></span>);
+    }
+    function listAliases(character) {
+        return character.aliases.map((alias)=><li key={alias.name}>{alias.name}, {alias.count}</li>);
+    }
+
+    function listCharacters() {
+        return docData.characters.map((char) => <p key={char.common_name}><h3>{char.common_name}</h3>
+            Full Name: {char.full_name}<br/>
+            Count: {char.count}<br/>
+            Gender: {renderGender(char.gender)}<br/>
+            Aliases: {listAliases(char)}
+        </p>);
+    }
+
 
     useEffect(() => {
         fetch(`/api/document/${id}`)
@@ -18,6 +36,7 @@ const SingleDocument = ({id}) => {
     }, []);
 
     const handleShowText = () => setShowText(prevShowText => !prevShowText);
+    const handleGetCharacters = () => setGetCharacters(prevGetCharacters => !prevGetCharacters);
 
     return (
         <div className="container-fluid">
@@ -31,6 +50,12 @@ const SingleDocument = ({id}) => {
                         Year Published {docData.year ? docData : "Unknown"}
                         <br/>
                         Word Count: {docData.word_count.toLocaleString()}
+                    </p>
+                    <p>
+                        <button className="btn btn-outline-primary mb3" onClick={handleGetCharacters}>
+                        {getCharacters ? "Hide Characters" : "Show Characters"}
+                        </button>
+                        {getCharacters && <p className={STYLES.docText}>{listCharacters()}</p>}
                     </p>
                     <button className="btn btn-outline-primary mb-3" onClick={handleShowText}>
                         {showText ? "Hide Full Text" : "Show Full Text"}
