6783a71e5cf1c0bbd5f4d1f58cd381df7a2a04a9
Peihua Huang <nemesis2126@gmail.com>
2021-07-12 15:53:10 -0400

    set up corpora component and added it to index.js

---
+++ frontend/components/Corpora.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import STYLES from "./Corpora.module.scss";
+import {getCookie} from "../common";
+
+const Corpora = () => {
+
+};
+
+export default Corpora;
