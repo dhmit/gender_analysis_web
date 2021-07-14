593547ef3ade72147712dd8d819101e94fc17120
Peihua Huang <nemesis2126@gmail.com>
2021-07-14 15:46:04 -0400

    added list of documents that can be added to corpus

--- frontend/components/Corpus.js
+++ frontend/components/Corpus.js
@@ -6,16 +6,79 @@ import {getCookie} from "../common";
 const Corpus = ({id}) => {
 
     const [corpusData, setCorpusData] = useState({});
+    const [allDocData, setAllDocData] = useState([]);
+    const [docCheckbox, setDocCheckbox] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch(`/api/corpus/${id}`)
-          .then(response => response.json())
-          .then(data => {
-              setCorpusData(data);
-              setLoading(false);
-          });
+            .then(response => response.json())
+            .then(data => {
+                setCorpusData(data);
+            });
+        fetch("/api/all_documents")
+            .then(response => response.json())
+            .then(data => {
+                setAllDocData(data);
+                data.map((doc) => setDocCheckbox((values) => ({
+          		    ...values,
+          		    [doc.id]: corpusData.documents.include(doc.id)
+          	    })));
+                setLoading(false);
+            });
     }, []);
+
+    const handleCheckBoxChange = (event) => {
+      setDocCheckbox((values) => ({
+          ...values,
+          [event.target.id]: !docCheckbox[event.target.id]
+      }));
+    };
+
+    const updateDocs = (event) => {
+        event.preventDefault();
+        const docList = [];
+        Object.entries(docCheckbox).map(([id, value]) => {
+            if (value) docList.push(id);
+        });
+        const csrftoken = getCookie("csrftoken");
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": csrftoken
+            },
+            body: JSON.stringify({
+                id: corpusData.id,
+                documents: docList
+            })
+        };
+        fetch("api/update_corpus_docs", requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                setCorpusData(data);
+            });
+    };
+
+    const allDocsList = () => {
+        return (
+            <form onSubmit={updateDocs}>
+                <h3>Documents in Corpus:</h3>
+                {allDocData.map((doc, i) => (
+                    <div key={i} className="custom-control custom-checkbox">
+                        <input type="checkbox"
+                            className="custom-control-input"
+                            id={doc.id} checked={docCheckbox[doc.id]}
+                            onChange={handleCheckBoxChange}/>
+                        <label className="custom-control-label"
+                            htmlFor={doc.id}>
+                            {doc.title}, by {doc.author} {doc.year ? `(${doc.year})` : null}</label>
+                    </div>
+                ))}
+                <button className="btn btn-primary" type="submit">Update</button>
+            </form>
+        );
+    };
 
     return (
         <div className="container-fluid">
@@ -25,6 +88,7 @@ const Corpus = ({id}) => {
                     <h1>{corpusData.title}</h1>
                     <h6>Description</h6>
                     <p>{corpusData.description}</p>
+                    {allDocsList()}
                 </div>
             }
         </div>
