3dc3e147e0e6199ce145eb294dba90d470f6b5db
Ashar Farooq <ashar744@gmail.com>
2021-07-15 16:50:24 -0400

    Improved the `generate_token_counter` method
    Revised the output to be a dictionary mapping a part of speech to a Counter instance instead of just a Counter instance. Removed the need to call pos_tag.
    
    Co-Authored-By: irinazoccolini <71565864+irinazoccolini@users.noreply.github.com>
    Co-Authored-By: Watermelanie <46464201+Watermelanie@users.noreply.github.com>
    Co-Authored-By: Joshua Feliciano <41080599+joshfeli@users.noreply.github.com>

--- backend/app/analysis/proximity.py
+++ backend/app/analysis/proximity.py
@@ -26,7 +26,7 @@ def run_analysis(corpus_id, word_window):
 
     for key in doc_ids:
         results[key] = generate_gender_token_counters(
-            Document.objects.values_list('tokenized_text', flat=True).filter(pk=key).get(),
+            Document.objects.values_list('part_of_speech_tags', flat=True).filter(pk=key).get(),
             genders,
             word_window
         )
@@ -34,12 +34,13 @@ def run_analysis(corpus_id, word_window):
     return results
 
 
-def generate_gender_token_counters(text, genders, word_window):
+def generate_gender_token_counters(pos_tags, genders, word_window):
     """
     Generates a dictionary mapping `Gender`s to a word count of words within a specified window of the `Gender`'s
     pronouns.
 
-    :param text: A list of strings that represents a tokenized text
+    :param pos_tags: A list of 2-element tuples: the first element is a word (str), and the second element is a
+        part-of-speech tag (str).
     :param genders: A set of Gender objects
     :param word_window: An integer describing the number of words to look at of each side of a gendered word
 
@@ -50,37 +51,37 @@ def generate_gender_token_counters(text, genders, word_window):
     results = {}
 
     for gender in genders:
-        doc_result = generate_token_counter(text, gender, word_window)
+        doc_result = generate_token_counter(pos_tags, gender, word_window)
         results[gender.label] = doc_result
 
     return results
 
 
-def generate_token_counter(text, gender, word_window):
+def generate_token_counter(pos_tags, gender, word_window):
     # pylint: disable=too-many-locals
     """
     Generates a 'Counter' instance mapping words to their frequency within a text.
 
-    :param text: A list of strings that represents a tokenized text
+    :param pos_tags: A list of 2-element tuples: the first element is a word (str), and the second element is a
+        part-of-speech tag (str).
     :param gender: A `Gender` object
     :param word_window: An integer describing the number of words to look at on each side of a gendered word
 
-    :return: A 'Counter' instance showcasing the numbered occurrences of words around a gendered pronoun
+    :return: A 'Dict' instance mapping the part of speech tag to a 'Counter' instance,
+        which features the numbered occurrences of words around a gendered pronoun
 
     """
 
-    output = Counter()
+    output = {}
 
-    for words in windowed(text, 2 * word_window + 1):
-        if words[word_window].lower() in gender.pronouns:
-            words = list(words)
+    for tagged_tokens in windowed(pos_tags, 2 * word_window + 1):
+        if tagged_tokens[word_window][0].lower() in gender.pronouns:
 
-            for index, word in enumerate(words):
-                words[index] = word.lower()
+            for index, tagged_token in enumerate(tagged_tokens):
+                word = tagged_token[0].lower()
+                pos_tag = tagged_token[1]
 
-            tagged_tokens = nltk.pos_tag(words)
-            for tag_index, _ in enumerate(tagged_tokens):
-                word = words[tag_index]
-                output[word] += 1
+                output.setdefault(pos_tag, Counter())
+                output[pos_tag][word] += 1
 
     return output
