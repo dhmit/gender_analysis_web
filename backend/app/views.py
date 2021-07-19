ffc986efe4cedca9982b8403e424c9505b4a8784
jaslarus <85576329+j-aslarus@users.noreply.github.com>
2021-07-19 15:41:27 -0400

    Create API endpoint to delete a document

--- backend/app/views.py
+++ backend/app/views.py
@@ -112,6 +112,17 @@ def add_document(request):
     return Response(serializer.data)
 
 
+@api_view(['POST'])
+def delete_document(request):
+    """
+    API endpoint for deleting a document
+    """
+    doc_id = request.data['id']
+    deleted_doc = Document.objects.get(id=doc_id)
+    res = deleted_doc.delete()
+    return Response(res)
+
+
 @api_view(['GET'])
 def all_documents(request):
     """
